8e421163af4644914d3d54849e90714984ba4128
Danielle <daniellerodris@gmail.com>
2020-04-22 14:38:57 -0300

    Requisição Post e widget Expanded

--- lib/Home.dart
+++ lib/Home.dart
@@ -13,7 +13,7 @@ class _HomeState extends State<Home> {
 
   String _urlBase = "https://jsonplaceholder.typicode.com";
 
-  Future<List<Post>> _recuperarPostagens() async{
+  Future<List<Post>> _recuperarPostagens() async {
     http.Response response = await http.get(_urlBase + "/posts");
 
     var dadosJson = json.decode(response.body);
@@ -28,44 +28,103 @@ class _HomeState extends State<Home> {
     //print(postagens.toString());
   }
 
+  _post() async {
+
+    var corpo = json.encode(
+      {
+        "userId": 120,
+        "id": null,
+        "title": "Título",
+        "body": "Corpo da postagem"
+      }
+    );
+
+    http.Response response = await http.post(
+      _urlBase + "/posts",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      body: corpo
+    );
+
+    print("resposta: ${response.statusCode}");
+    print("resposta: ${response.body}");
+  }
+
+  _put() {
+
+  }
+
+  _patch() {
+
+  }
+
+  _delete() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Consumo de serviço avançado"),
       ),
-      body: FutureBuilder<List<Post>>(
-        future: _recuperarPostagens(),
-        builder: (context, snapshot) {
-          switch(snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-              break;
-            case ConnectionState.active:
-            case ConnectionState.done:
-              if(snapshot.hasError) {
-                print("Lista: erro ao carregar.");
-              } else {
-                print("Lista: carregou.");
-                return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) {
-                    List<Post> lista = snapshot.data;
-                    Post post = lista[index];
-                    return ListTile(
-                      title: Text(post.title),
-                      subtitle: Text(post.body),
-                    );
-                  },
-                );
-              }
-              break;
-          }
-        },
-      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("Salvar"),
+                  onPressed: _post,
+                ),
+                RaisedButton(
+                  child: Text("Atualizar"),
+                  onPressed: _post,
+                ),
+                RaisedButton(
+                  child: Text("Deletar"),
+                  onPressed: _post,
+                )
+              ],
+            ),
+            Expanded(
+              child: FutureBuilder<List<Post>>(
+                future: _recuperarPostagens(),
+                builder: (context, snapshot) {
+                  switch(snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                      break;
+                    case ConnectionState.active:
+                    case ConnectionState.done:
+                      if(snapshot.hasError) {
+                        print("Lista: erro ao carregar.");
+                      } else {
+                        return ListView.builder(
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (context, index) {
+                            List<Post> lista = snapshot.data;
+                            Post post = lista[index];
+                            return ListTile(
+                              title: Text(post.title),
+                              subtitle: Text(post.body),
+                            );
+                          },
+                        );
+                      }
+                      break;
+                  }
+                },
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
